3b8d1f4d53b4212f8ab5c8e7924f533fb94f4afc
kartinils <149855735+kartinils@users.noreply.github.com>
2025-12-19 16:24:40 +0700

    Add files via upload

---
+++ app (3).py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Prediksi Harga BBCA", layout="wide")
+
+st.title("📈 Prediksi Harga Saham BBCA")
+st.write("""
+Selamat datang di aplikasi prediksi harga saham BBCA menggunakan model **LSTM**.
+
+Gunakan menu di sidebar kiri untuk:
+- 🔮 Melakukan prediksi harga n-hari ke depan  
+- 📊 Melihat grafik saham dengan visualisasi interaktif
+""")
